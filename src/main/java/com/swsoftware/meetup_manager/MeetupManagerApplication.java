16c12077662e0303eda79a18fd973a5cc2a1b90b
SWillames <sergiowoc@gmail.com>
2024-10-18 16:52:58 -0300

    first commit.

---
+++ src/main/java/com/swsoftware/meetup_manager/MeetupManagerApplication.java
@@ -0,0 +1,13 @@
+package com.swsoftware.meetup_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swsoftware/meetup_manager/MeetupManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swsoftware.meetup_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
